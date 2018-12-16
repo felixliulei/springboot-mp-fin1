82d54f0566b2089d229320ca05cd6ece07174af7
felixliulei <763838587@qq.com>
2018-12-16 17:10:30 +0800

    首次提交

---
+++ src/main/java/com/seawaterbt/ssm/service/TeacherService.java
@@ -0,0 +1,8 @@
+package com.seawaterbt.ssm.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.seawaterbt.ssm.entity.Teacher;
+
+
+public interface TeacherService extends IService<Teacher> {
+}
